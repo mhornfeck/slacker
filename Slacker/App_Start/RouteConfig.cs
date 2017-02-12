7cfa5580ae8d8582ae296efd6589fb5a9fbd0719
Michael Hornfeck <mhornfeck@clearcompany.com>
2017-02-12 10:47:25 -0500

    initial pass at sending/receiving messages from the hub

--- Slacker/App_Start/RouteConfig.cs
+++ Slacker/App_Start/RouteConfig.cs
@@ -12,12 +12,7 @@ namespace Slacker
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Chat", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
