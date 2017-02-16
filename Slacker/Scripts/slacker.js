78e010bd88f9b80b3d569781c3fcce3b4ce5400b
Michael Hornfeck <mhornfeck@clearcompany.com>
2017-02-15 22:18:11 -0500

    added client methods for join/leave

--- Slacker/Scripts/slacker.js
+++ Slacker/Scripts/slacker.js
@@ -7,6 +7,14 @@
         $('.js-discussion-list').append('<li><strong>' + name + '</strong>: ' + message + '</li>');
     };
 
+    chat.client.userJoinedChannel = function (userName) {
+        $('.js-discussion-list').append('<li><strong>' + userName + '</strong> joined the party.</li>');
+    };
+
+    chat.client.userLeftChannel = function (userName) {
+        $('.js-discussion-list').append('<li><strong>' + userName + '</strong> left the party.</li>');
+    };
+
     // Create a function to handle sending messages to the hub
     var sendChatMessage = function() {
         chat.server.sendMessage('@' + $('.js-username-input').val(), $('.js-message-input').val());
