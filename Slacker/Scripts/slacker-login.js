994dce13bb00b22c308416f80bf8d291b4801fb6
Michael Hornfeck <mhornfeck@clearcompany.com>
2017-02-15 22:32:22 -0500

    added join channel links to UI

--- Slacker/Scripts/slacker-login.js
+++ Slacker/Scripts/slacker-login.js
@@ -1,21 +1,17 @@
 ﻿$(function () {
     $('.js-username-input').focus();
 
-    var setUsername = function () {
+    var setUsername = function (channelName) {
+        $('.js-channel-name-hidden').val(channelName);
+        $('.js-channel-label').text(channelName);
         $('.js-username-label').text('@' + $('.js-username-input').val());
         $('.js-login-container').toggleClass('hidden', true);
         $('.js-chat-container').toggleClass('hidden', false);
         $('.js-message-input').focus();
     }
 
-    $('.js-username-input').keydown(function (event) {
-        // enter has keyCode = 13, change it if you want to use another button
-        if (event.keyCode === 13) {
-            setUsername();
-        }
-    });
-
-    $('.js-btn-set-username').click(function () {
-        setUsername();
+    $('.js-channel').click(function (e) {
+        var $target = $(e.currentTarget);
+        setUsername($target.text());
     });
 });
